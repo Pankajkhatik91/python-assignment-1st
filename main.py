62b1b6786fc3af19a715bed1871d0993c95baad4
pankajkhatik91 <pankajkhatik91@gmail.com>
2025-09-29 12:09:55 +0530

    assingment 1st

---
+++ main.py
@@ -0,0 +1,42 @@
+#task 1
+print("Task-1")
+
+a=input("enter the first number")
+b=input("enter the second number")
+
+a=int(a)
+b=int(b)
+
+
+print("addition", (a+b))
+print("subtraction", (a-b))
+print("multiplication", (a*b))
+print("division", (a/b))
+
+print("....................")
+:q!
+
+
+
+#task 2
+
+print("Task-2")
+
+a=input("enter your first name: ")
+
+b=input("enter your last name: ")
+
+
+print(":)")
+
+
+print("Hello, " + a +" " + b + "! Welcome to the python program.")
+
+
+
+
+
+
+
+
+
